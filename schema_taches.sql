d64c4d07506696c27a4b3ae136f4e6bfebadd51d
Pasidi <167764476+Pasidi-hash@users.noreply.github.com>
2026-02-18 16:07:23 +0100

    Delete schema_taches.sql

--- schema_taches.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS taches;
-
-CREATE TABLE taches (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    titre TEXT NOT NULL,          -- Correspond à name="titre"
-    description TEXT NOT NULL,    -- Correspond à name="description"
-    date_echeance TEXT NOT NULL,  -- Correspond à name="dateEcheance"
-    statut INTEGER DEFAULT 0      -- 0 = En cours, 1 = Terminé
-);
